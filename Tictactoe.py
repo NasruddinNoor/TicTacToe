b6b960ab7e18006cf8b748e9c96560e955a8545c
Junaid Qadir <shekhanzai.baloch@gmail.com>
2020-06-08 19:42:25 +0500

    Add isHorizontalTop method Rename isDraw to isBoardFull BugFix in isHorizontalMiddle and isHorizontalTop Optimize move method Add tests

--- Tictactoe.py
+++ Tictactoe.py
@@ -22,11 +22,15 @@ class TicTacToe():
             self.isInvalidMove = False
         else:
             self.togglePlayer()
-        if self.isDraw():
-            print("The match is a draw!")
-        elif self.isWon():
+
+        if  self.isWon():
             self.isGameOver = True
             print("Player " + self.player + " has won!")
+        elif self.isBoardFull() and not self.isWon():
+            self.isGameOver = True
+            print("The game is draw")
+        else:
+            print(self.player + "'s move")
 
     def togglePlayer(self):
         if self.player == "P1":
@@ -42,15 +46,15 @@ class TicTacToe():
 
             self.isInvalidMove = True
 
-    def isDraw(self):
-        isdraw = True
+    def isBoardFull(self):
+        isBoardFull = True
 
         for row in self.board:
             for cell in row:
-                if row[cell] != 0:
-                    isdraw = False
+                if row[cell] == 0:
+                    isBoardFull = False
 
-        return isdraw
+        return isBoardFull
 
     def isDiagonalLeft(self):
         isDiagonal = True
@@ -182,9 +186,85 @@ class TicTacToe():
             rowIndex = rowIndex + 1
         return row1Cell3 and row2Cell6 and row3Cell9
 
+    def isHorizontalTop(self):
+
+        rowIndex = 1
+        cellIndex = 1
+
+        row1Cell1 = False
+        row1Cell2 = False
+        row1Cell3 = False
+
+        for row in self.board:
+
+            cellIndex = 1
+
+            for cell in row.items():
+                if rowIndex == 1 and cellIndex == 1:
+                    row1Cell1 = cell[1] == self.player
+                elif rowIndex == 1 and cellIndex == 2:
+                    row1Cell2 = cell[1] == self.player
+                elif rowIndex == 1 and cellIndex == 3:
+                    row1Cell3 = cell[1] == self.player
+                cellIndex = cellIndex + 1
+
+            rowIndex = rowIndex + 1
+        return row1Cell1 and row1Cell2 and row1Cell3
+
+    def isHorizontalMiddle(self):
+
+        rowIndex = 1
+        cellIndex = 1
+
+        row2Cell1 = False
+        row2Cell2 = False
+        row2Cell3 = False
+
+        for row in self.board:
+
+            cellIndex = 1
+
+            for cell in row.items():
+                if rowIndex == 2 and cellIndex == 1:
+                    row2Cell1 = cell[1] == self.player
+                elif rowIndex == 2 and cellIndex == 2:
+                    row2Cell2 = cell[1] == self.player
+                elif rowIndex == 2 and cellIndex == 3:
+                    row3Cell3 = cell[1] == self.player
+                cellIndex = cellIndex + 1
+
+            rowIndex = rowIndex + 1
+        return row2Cell1 and row2Cell2 and row2Cell3
+
+    def isHorizontalBottom(self):
+
+        rowIndex = 1
+        cellIndex = 1
+
+        row3Cell1 = False
+        row3Cell2 = False
+        row3Cell3 = False
+
+        for row in self.board:
+
+            cellIndex = 1
+
+            for cell in row.items():
+                if rowIndex == 3 and cellIndex == 1:
+                    row3Cell1 = cell[1] == self.player
+                elif rowIndex == 3 and cellIndex == 2:
+                    row3Cell2 = cell[1] == self.player
+                elif rowIndex == 3 and cellIndex == 3:
+                    row3Cell3 = cell[1] == self.player
+                cellIndex = cellIndex + 1
+
+            rowIndex = rowIndex + 1
+        return row3Cell1 and row3Cell2 and row3Cell3
+
     def isWon(self):
         return self.isDiagonalLeft() or self.isDiagonalRight() \
-               or self.isVerticalLeft() or self.isVerticalCenter() or self.isVerticalRight()
+               or self.isVerticalLeft() or self.isVerticalCenter() or self.isVerticalRight() \
+               or self.isHorizontalTop() or self.isHorizontalMiddle() or self.isHorizontalBottom()
 
     def resetBoard(self):
         self.isGameOver = False
@@ -259,9 +339,50 @@ def testP1WinnerRightVertical():
     tictac.move("cell6")  # P2
 
 
-def testDraw():
+def testP1WinnerTopHorizontal():
+    print("test P1 Winner Top Horizontal")
+
+    tictac.move("cell1")  # P1
+    tictac.move("cell4")  # P2
+
     tictac.move("cell2")  # P1
+    tictac.move("cell5")  # P2
+
+    tictac.move("cell3")  # P1
+    tictac.move("cell6")  # P2D
+
+
+def testP1WinnerMiddleHorizontal():
+    print("test P1 Winner Middle Horizontal")
+
+    tictac.move("cell4")  # P1
     tictac.move("cell1")  # P2
+
+    tictac.move("cell5")  # P1
+    tictac.move("cell2")  # P2
+
+    tictac.move("cell6")  # P1
+    tictac.move("cell3")  # P2D
+
+
+def testP1WinnerBottomHorizontal():
+    print("test P1 Winner Bottom Horizontal")
+
+    tictac.move("cell7")  # P1
+    tictac.move("cell1")  # P2
+
+    tictac.move("cell8")  # P1
+    tictac.move("cell2")  # P2
+
+    tictac.move("cell9")  # P1
+    tictac.move("cell3")  # P2
+
+
+def testDraw():
+    print("test Draw")
+
+    tictac.move("cell1")  # P1
+    tictac.move("cell2")  # P2
 
     tictac.move("cell3")  # P1
     tictac.move("cell4")  # P2
@@ -270,14 +391,30 @@ def testDraw():
     tictac.move("cell6")  # P2
 
     tictac.move("cell7")  # P1
-    tictac.move("cell9")  # P2
     tictac.move("cell8")  # P1
+    tictac.move("cell9")  # P2
 
 
-# testP1WinnerLeftDiagonal()
-# tictac.resetBoard()
-# testP1WinnerRightDiagonal()
 tictac.resetBoard()
-# testP1WinnerLeftVertical()
+testP1WinnerLeftDiagonal()
+
+tictac.resetBoard()
+testP1WinnerRightDiagonal()
+
+tictac.resetBoard()
+testP1WinnerLeftVertical()
+
 tictac.resetBoard()
 testP1WinnerCenterVertical()
+
+tictac.resetBoard()
+testP1WinnerTopHorizontal()
+
+tictac.resetBoard()
+testP1WinnerMiddleHorizontal()
+
+tictac.resetBoard()
+testP1WinnerBottomHorizontal()
+
+# tictac.resetBoard()
+# testDraw()
